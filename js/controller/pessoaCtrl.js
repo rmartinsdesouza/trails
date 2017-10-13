900e2a4f1d5de923138ed1a39becc492b7e050b4
Rodrigo Martins de Souza <rmartinsdesouza@gmail.com>
2017-10-13 19:02:05 -0400

    Post Pessoa

--- js/controller/pessoaCtrl.js
+++ js/controller/pessoaCtrl.js
@@ -7,7 +7,7 @@
 	    .module('app')
 	    .controller('pessoaCtrl', pessoaCtrl);
 
-	function pessoaCtrl($scope, $timeout, pessoaSvc){
+	function pessoaCtrl($scope, $timeout, pessoaSvc, $http){
 
 		var vm = this;
 		vm.form = {
@@ -33,8 +33,8 @@
 			vm.form.pessoa = angular.copy(item);
 		}
 
-		vm.savePessoa = function(){
-			console.log('Pessoa salva.', vm.form.pessoa)
+		vm.savePessoa = function(NOME,CPF,DATA_NASCIMENTO,LOGIN_CODIGO){
+			// console.log('Pessoa salva.', vm.form.pessoa)
 			// try {
 			// 	pessoaSvc.savePessoa().then(function(result){
 					
@@ -44,6 +44,39 @@
 			// catch(err) {
 			// 	getListPessoa();
 			// }	
+			// $scope.DESCRICAO = null;
+			$scope.NOME = null;
+			$scope.CPF = null;
+			$scope.DATA_NASCIMENTO = null;
+			$scope.LOGIN_CODIGO = null;
+
+		var data = {
+		// DESCRICAO: DESCRICAO
+		
+			NOME: NOME,
+			CPF: CPF,
+			DATA_NASCIMENTO: DATA_NASCIMENTO,
+			LOGIN_CODIGO: LOGIN_CODIGO
+
+			// NOME: 'NOME',
+			// CPF: 'CPF',
+			// DATA_NASCIMENTO: 'DATA_NASCIMENTO',
+			// LOGIN_CODIGO: 'LOGIN_CODIGO'
+		};
+	
+		$http.post('http://bedrock/api/pessoa', data).then(function (response, $http) {
+
+
+		if (response.data)
+			$scope.msg = "Post Data Submitted Successfully!";
+			getListPessoa();
+		}, function (response) {		
+			$scope.msg = "Service not Exists";
+			$scope.statusval = response.status;
+			$scope.statustext = response.statusText;
+			// $scope.headers = response.headers();
+		});
+		
 		}
 
 		getListPessoa();
